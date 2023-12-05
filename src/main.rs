883bb2927ea01b5c3414bf6057eff1918ed735e2
kinire98 <iknitgarri@outlook.com>
2023-12-05 11:18:09 +0100

    Primer commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    let thing = std::fs::read_dir(r"C:\Users\Iknit\Documents").unwrap();
+    for path in thing {
+        println!("{:?}", path.unwrap().path());
+    }
 }
