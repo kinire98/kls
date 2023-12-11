3414967922ebae27a38010af595866b7ae786bcc
kinire98 <iknitgarri@outlook.com>
2023-12-11 10:35:46 +0100

    fuck windows

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use std::io;
+use std::fs;
+use std::os::windows::prelude::*;
+
+
+
+
 
 
 fn main() {
-    let thing = std::fs::read_dir(r"C:\Users\Iknit\Documents").unwrap();
+    let thing = std::fs::read_dir(r".\").unwrap();
     for path in thing {
-        println!("{:?}", path.unwrap().path());
+        let jeje = path.unwrap().path().display().to_string();
+        let metadata = fs::metadata(jeje.clone()).unwrap();
+        let atts = metadata.file_attributes();
+        let file_size = metadata.file_size();
+        println!("{}: {}, {}",jeje,  atts, file_size);
     }
 }
