b18b40a74326c6df01c5f6dc016a9b54c46f3490
kinire98 <iknitgarri@outlook.com>
2023-12-11 12:13:09 +0100

    added some default behaviour

---
+++ README.md
@@ -0,0 +1,3 @@
+# kls
+ls clone written in Rust
+Still in a very basic and minimal version.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 #[cfg(windows)]
 use std::os::windows::prelude::*;
 #[cfg(unix)]
 use std::os::linux::fs::MetadataExt;
 use clap::*;
 
+use std::env::current_dir;
+
 #[derive(Parser)]
 struct Args {
-    path: String
+    path: Option<PathBuf>
 }
 
 
 
 fn main() {
     let args = Args::parse();
-    let thing = std::fs::read_dir(args.path).unwrap();
+    let mut path = if let Some(x) = args.path {
+        x
+    } else {
+        PathBuf::new()
+    };
+    if path == PathBuf::new() {
+        path = current_dir().unwrap();
+    }
+    println!("{:?}", path);
+    
+    let thing = std::fs::read_dir(path).unwrap();
     for path in thing {
         let jeje = path.unwrap().path().display().to_string();
         let metadata = fs::metadata(jeje.clone()).unwrap();
