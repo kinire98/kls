2329de04b3d2ea50251bf7adcf5391576853b164
kinire98 <iknitgarri@outlook.com>
2023-12-11 11:06:41 +0100

    leleel

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 /*
 
-Docs for attributes constants for files
+? Docs for attributes constants for files in Windows
 File attribute constant: 
 https://learn.microsoft.com/en-us/windows/win32/fileio/file-attribute-constants
 https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/file-attribute-constants
@@ -26,4 +26,25 @@ For linux there is a crate file_mode:
 https://docs.rs/file-mode/latest/file_mode/
 https://crates.io/crates/file-mode
 Saves me
+*/
+
+
+
+/*
+? To investigate properties
+https://doc.rust-lang.org/std/fs/struct.Metadata.html
+https://doc.rust-lang.org/std/os/windows/fs/trait.MetadataExt.html#tymethod.file_attributes
+
+*/
+
+/*
+? Flags to implement
+  -a, --all                  do not ignore entries starting with .
+  -A, --almost-all           do not list implied . and ..
+      --author               with -l, print the author of each file
+  -B, --ignore-backups       do not list implied entries ending with ~
+  -d, --directory            list directories themselves, not their contents
+  -l                         use a long listing format
+  -R, --recursive            list subdirectories recursively
+  -s, --size                 print the allocated size of each file, in blocks
 */
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use std::io;
 use std::fs;
 #[cfg(windows)]
 use std::os::windows::prelude::*;
+#[cfg(unix)]
+use std::os::linux::fs::MetadataExt;
+use clap::*;
 
-
-
+#[derive(Parser)]
+struct Args {
+    path: String
+}
 
 
 
 fn main() {
-
-    #[cfg(windows)]
-    let thing = std::fs::read_dir(r".\").unwrap();
-    #[cfg(unix)]
-    let thing = std::fs::read_dir(r"./").unwrap();
+    let args = Args::parse();
+    let thing = std::fs::read_dir(args.path).unwrap();
     for path in thing {
         let jeje = path.unwrap().path().display().to_string();
         let metadata = fs::metadata(jeje.clone()).unwrap();
@@ -26,7 +27,7 @@ fn main() {
         #[cfg(unix)]
         {
             let atts = metadata.file_type();
-            let file_size = metadata.file_type();
+            let file_size = metadata.st_blocks();
             println!("{}: {:?}, {:?}",jeje,  atts, file_size);
         }
     }
